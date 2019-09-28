c1e69b46bddd686f59b291d4266fc39df5d323ed
Leo Dion <leogdion@brightdigit.com>
2019-09-28 17:06:47 -0400

    Initial Commit

---
+++ xcodegen-sample-watch-app WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcodegen-sample-watch-app WatchKit Extension
+//
+//  Created by Leo Dion on 9/28/19.
+//  Copyright © 2019 BrightDigit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcodegen-sample-watch-app WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  xcodegen-sample-watch-app WatchKit Extension
+//
+//  Created by Leo Dion on 9/28/19.
+//  Copyright © 2019 BrightDigit. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
